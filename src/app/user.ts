4380f17299d4dd4753cb2b4a0848f2fac7af668d
Brian <bweloba@gmail.com>
2021-04-06 04:47:12 +0300

    add classes

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,14 +1,16 @@
 export class User {
 
   constructor(
-    public name: string,
-    public username: string,
-    public details: string,
-    public repos: string,
-    public followers: number,
-    public following: number,
-    public location: string,
-    public email: string,
-    public url: string
-  ){}
+    public avatar_url: any,
+    public name: any,
+    public login: any,
+    public bio: any,
+    public public_repos: any,
+    public followers: any,
+    public following: any,
+    public location: any,
+    public email: any,
+    public html_url: any,
+    public created_at: any
+  ) { }
 }
