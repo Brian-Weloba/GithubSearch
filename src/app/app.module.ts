0ea814c826f4cf93f4890fb066a2ecdbbf0cd24e
Brian <bweloba@gmail.com>
2021-04-08 19:23:37 +0300

    import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatCardModule } from '@angular/material/card'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormComponent } from './form/form.component'
 import { FormsModule } from '@angular/forms';
 import { RoutingModule } from './routing/routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ReposComponent } from './repos/repos.component';
+import { RepoFormComponent } from './repo-form/repo-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
     FormComponent,
+    ReposComponent,
+    RepoFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
