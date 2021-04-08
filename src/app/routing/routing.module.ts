b6a5276940e7a13b2088bd3a599c4e4c0956e2b1
Brian <bweloba@gmail.com>
2021-04-08 19:27:23 +0300

    update routes

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,9 +1,11 @@
+import { ReposComponent } from './../repos/repos.component';
 import { ProfileComponent } from './../profile/profile.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
+  {path: 'repositories', component: ReposComponent},
   { path: '', redirectTo: "/profile", pathMatch: "full" },
 ]
 
